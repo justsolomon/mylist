8aaed7ede0452ed46c13ec47e095f90e5a8e49a3
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2021-06-20 15:25:04 +0100

    Update API base url

--- src/utils/AxiosBase/index.ts
+++ src/utils/AxiosBase/index.ts
@@ -3,7 +3,7 @@ import store from "../../redux/store";
 
 // Creates a base instance for all axios based request
 const instance = axios.create({
-  baseURL: `https://mylist-api.vercel.app`,
+  baseURL: `https://mylist-app.herokuapp.com`,
   withCredentials: true,
 });
 
